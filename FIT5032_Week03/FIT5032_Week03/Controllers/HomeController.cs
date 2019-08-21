8ad386e8a33b8cfab13dd77d7225d01a2fba51cd
swathy <swat0017@student.monash.edu>
2019-08-21 18:51:17 +1000

    Repository week3

---
+++ FIT5032_Week03/FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using FIT5032_Week03.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // ViewBag.Message = "Your application description page.";
+            Hello hello = new Hello();
+            // I assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
